1b3245e17ea91c8843b170743b1f5bedbd681f38
ModernNeesh <86896359+ModernNeesh@users.noreply.github.com>
2026-04-21 18:59:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ from pathlib import Path
 import yaml
 import logging
 
-
+#Load configs
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
+#Start logging
 logging.basicConfig(filename='logging/app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+#Get access token and multiplayer games data
 ACCESS_TOKEN = get_access_token()
 df = get_multiplayer_games(ACCESS_TOKEN)
+#Process data
 df = split_list_columns(df, ACCESS_TOKEN)
 
-
+#Save cleaned data
 if len(df) > 0:
     clean_multiplayer_games_fp = Path(config['multiplayer_games_folder'] + config['multiplayer_games_clean_fp'])
     logging.info("Saving cleaned multiplayer games dataframe to: %s", clean_multiplayer_games_fp)
